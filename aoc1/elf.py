55370806f4bd3ad02c4ec8826f360782d20b34af
coffee_tp <notreally@somemail.com>
2023-12-03 12:49:35 +0100

    Fixed variable names to enforce naming convention.

--- aoc1/elf.py
+++ aoc1/elf.py
@@ -3,13 +3,13 @@ class Elf:
     food_bag = [0]
     total_calories_carried = 0
 
-    def __init__(self, foodbag):
-        self.food_bag = foodbag
+    def __init__(self, food_bag):
+        self.food_bag = food_bag
         self.sum_up_calories()
 
     def sum_up_calories(self):
         self.total_calories_carried = sum(self.food_bag)
 
-    def add_food_item(self, fooditem):
-        self.food_bag.append(fooditem)
+    def add_food_item(self, food_item):
+        self.food_bag.append(food_item)
         self.sum_up_calories()

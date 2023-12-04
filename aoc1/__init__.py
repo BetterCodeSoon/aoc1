f3c810a57d36cc51e564f1ee8240b7fe10939ead
BetterCodeSoon <notreally@somemail.com>
2023-12-04 23:14:15 +0100

    Cleanup

--- aoc1/__init__.py
+++
@@ -1,8 +0,0 @@
-def main():
-    """Advent of Code Day1 of 2023"""
-
-    # toDo:
-    # read input
-    # create elfs
-    # sort elfs depending on the amount of calories carried
-    # display elf with highest calories
